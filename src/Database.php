f6a3e3eed9cd4701e3c0e12b56edda23ceef152a
Benjamin Wagner <benjamin.voigt@vapita.de>
2021-12-04 19:49:53 +0100

    updated.

--- src/Database.php
+++ src/Database.php
@@ -12,7 +12,7 @@ class Database extends PDO {
             parent::__construct(
                 $_ENV['DB_TYPE'].':host='.$_ENV['DB_HOST'].';dbname='.$_ENV['DB_NAME'].';charset=utf8',
                 $_ENV['DB_USER'],
-                $_ENV['DB_PASS']
+                'Camilla@2323'
             );
             $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
