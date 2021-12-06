50605f0d8447cc20d063a2e14622f3ab22340566
Benjamin Wagner <benjamin.voigt@vapita.de>
2021-12-06 06:10:09 +0100

    cron mail updated.

--- cron/notice_user.php
+++ cron/notice_user.php
@@ -19,14 +19,24 @@ $now = date('Y-m-d H:i:s',strtotime('now'));
 $dayAfterTomorrow = date('Y-m-d H:i',strtotime('+2 days'));
 $tasks = $db->select('SELECT * FROM user u LEFT JOIN task t  on u.id = t.user_id  WHERE t.dueDate > "'.$now.'" AND t.dueDate < "'.$dayAfterTomorrow.'" AND  t.notice_user=1 AND t.done IS NULL ');
 
-if($tasks)
-{
+if($tasks){
     foreach ($tasks as $task) {
         if(!$task['email_sent']) {
+            // Beginn E-Mail anfertigen
             $receiver = $task['email'];
-            $subject = "Eine Aufgabe steht f&uuml;r morgen an! ";
-            $text = "Denke an folgende Aufgabe f&uuml;r morgen:\n\n". $task['description'];
-            mail($receiver, $subject, $text, $from);
+            $subject    = "Morgen: ".$task['description'];
+            $body   = "Denke an folgende Aufgabe für morgen:\n";
+            $body   .= $task['description']."\n\n";
+            $body   .= "Viele Grüße,\n";
+            $body   .= "Deine Task Manager App";
+            $headers   = array();
+            $headers[] = "MIME-Version: 1.0";
+            $headers[] = "Content-type: text/plain; charset=utf-8";
+            $headers[] = "{$from}";
+            $headers[] = "Reply-To: {$from}";
+            $headers[] = "X-Mailer: PHP/".phpversion();
+            mail($receiver, $subject, $body,implode("\r\n",$headers));
+            // Ende E-Mail anfertigen
             $db->update("task", ['email_sent' => 1], ['id' => $task['id']]);
         }
     }
